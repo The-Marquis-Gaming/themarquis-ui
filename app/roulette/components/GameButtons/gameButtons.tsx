6675421cfa6cbb05c862f750bd35bb332ba0b17f
CarlosR <carlos.ramos@grupoimportia.com>
2023-11-19 00:02:06 +0300

    fix create and mint

--- app/roulette/components/GameButtons/gameButtons.tsx
+++ app/roulette/components/GameButtons/gameButtons.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import "./gameButtons.css";
 import { useDojo } from "../../../DojoContext";
@@ -9,10 +9,7 @@ interface GameButton {
   handleEraseClick: () => void;
 }
 
-
-
 function GameButtons(props: GameButton) {
-
   const {
     setup: {
       systemCalls: { mint },
@@ -20,35 +17,51 @@ function GameButtons(props: GameButton) {
       entityUpdates,
       network: { contractComponents, graphClient },
     },
-    account: { create, list, select, account, isDeploying, },
+    account: { create, list, select, account, isDeploying },
   } = useDojo();
 
-
-  const { clear, eraseMode, handleEraseClick } = props
+  const { clear, eraseMode, handleEraseClick } = props;
   const handleCreateAndMint = () => {
     create();
-    mint(account); 
   };
 
+  useEffect(() => {
+    if (account) {
+      mint(account);
+    }
+  }, [account]);
 
   return (
     <div className="flex gap-10">
-      <button onClick={handleCreateAndMint} className="py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px]">
+      <button
+        onClick={handleCreateAndMint}
+        className="py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px]"
+      >
         CREATE & MINT
       </button>
 
       <button
-        className={`py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px] button-eraser ${eraseMode ? 'erase-mode' : ''}`}
+        className={`py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px] button-eraser ${
+          eraseMode ? "erase-mode" : ""
+        }`}
         onClick={handleEraseClick}
       >
-        <Image src="/images/eraser.png" alt="eraser" width={29} height={29}></Image>
+        <Image
+          src="/images/eraser.png"
+          alt="eraser"
+          width={29}
+          height={29}
+        ></Image>
         ERASE
       </button>
-      <button className="py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px]"
+      <button
+        className="py-4 px-20 border border-solid border-[#A962FF] bg-[#111] rounded-[15px]"
         onClick={clear}
-      >CLEAR</button>
+      >
+        CLEAR
+      </button>
     </div>
-  )
+  );
 }
 
-export default GameButtons
+export default GameButtons;
