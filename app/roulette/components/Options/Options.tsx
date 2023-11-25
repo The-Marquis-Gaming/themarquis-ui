a7abae057d9bd9b3f2864237509a29ce29eb0562
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2023-11-25 00:16:26 -0500

    Modifying component options

--- app/roulette/components/Options/Options.tsx
+++ app/roulette/components/Options/Options.tsx
@@ -28,6 +28,54 @@ function Options(props:OptionsProps) {
       setData(updatedSlots);
     };
 
+    const handlearrayClick = (number: number) => {
+      setSelectedNumber(number);
+
+      const updatedSlots = slots.map((slot, index) => {
+        if (index >= 13 && index <= 24) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
+        }
+        return slot;
+      });
+      setData(updatedSlots);
+    };
+
+    const handlearrayfive = (number: number) => {
+      setSelectedNumber(number);
+
+      const updatedSlots = slots.map((slot, index) => {
+        if (index >= 25 && index <= 35) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
+        }
+        return slot;
+      });
+      setData(updatedSlots);
+    };
+
+    const handlearray = (number: number) => {
+      setSelectedNumber(number);
+
+      const updatedSlots = slots.map((slot, index) => {
+        if (index >= 1 && index <= 18) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
+        }
+        return slot;
+      });
+      setData(updatedSlots);
+    };
+
+    const handlearraytwo = (number: number) => {
+      setSelectedNumber(number);
+
+      const updatedSlots = slots.map((slot, index) => {
+        if (index >= 19 && index <= 35) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
+        }
+        return slot;
+      });
+      setData(updatedSlots);
+    };
+
     console.log(slots)
     return (
         <div className="option">
@@ -48,22 +96,41 @@ function Options(props:OptionsProps) {
 
           </div>
             <div className="flex">
-                <button className="w-[100px] text-center border border-solid border-white py-4">1-18</button>
+                <button className="w-[100px] text-center border border-solid border-white py-4"
+                onClick={()=> handlearray(1)}
+                >1-18</button>
                 <button className="w-[100px] text-center border border-solid border-white py-4">EVEN</button>
             </div>
         </div>
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
-            <button className="w-[200px] py-4">13-24</button>
+          <div>
+            <button className="w-[200px] py-4"
+            onClick={()=>handlearrayClick(1)}
+            >13-24</button>
+            {selectedNumber !== null && (
+              <div className="container-chips">
+                {slots[selectedNumber].coins.map((coin: any, index: number) => (
+                  <Chipsduplicate key={index} color={Color.White}>
+                    {String(coin)}
+                  </Chipsduplicate>
+                ))}
+              </div>
+            )}
+          </div>
             <div className="flex">
                 <button className="w-[100px] text-center border border-solid border-white bg-[#2B2A2A] py-4">BLACK</button>
                 <button className="w-[100px] text-center border border-solid border-white bg-[#561589] py-4">PURPLE</button>
             </div>
         </div>
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
-            <button className="w-[200px] py-4">25-35</button>
+            <button className="w-[200px] py-4"
+            onClick={()=>handlearrayfive(1)}
+            >25-35</button>
             <div className="flex">
                 <button className="w-[100px] text-center border border-solid border-white py-4">ODD</button>
-                <button className="w-[100px] text-center border border-solid border-white py-4">19-35</button>
+                <button className="w-[100px] text-center border border-solid border-white py-4"
+                onClick={()=>handlearraytwo(1)}
+                >19-35</button>
             </div>
         </div>
     </div>
