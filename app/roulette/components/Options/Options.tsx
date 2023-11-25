9c4e5585cffc1f438fddf2de80386c396070f4ff
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2023-11-25 13:15:11 -0500

    Adding functionality to the black button

--- app/roulette/components/Options/Options.tsx
+++ app/roulette/components/Options/Options.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Chipsduplicate from "../RouletteChips/Chips/Chipsduplicate";
 import { Color } from "../RouletteChips/Chips/Chipsduplicate";
 import './Options.css'
+import { ColorSlot } from "../RouletteNumber/SlotNumber";
 
 interface OptionsProps {
     slots: any[];
@@ -13,8 +14,8 @@ interface OptionsProps {
   
 function Options(props:OptionsProps) {
     const {slots , setData, valueChip , eraseMode} = props
-
     const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+    const [selectedColor, setSelectedColor] = useState<ColorSlot | null>(null);
 
     const handleButtonClick = (number: number) => {
       setSelectedNumber(number);
@@ -76,13 +77,28 @@ function Options(props:OptionsProps) {
       setData(updatedSlots);
     };
 
+    const handleButtonClickColor = (color: ColorSlot) => {
+      setSelectedColor(color);
+      const updatedSlots = slots.map((slot) => {
+        if (slot.color === ColorSlot.Gray) {
+          return { ...slot, coins: [...slot.coins,  valueChip ] };
+        }
+        
+       
+        return slot;
+      });
+
+      setData(updatedSlots);
+    };
+
+
     console.log(slots)
     return (
         <div className="option">
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
           <div className={`${eraseMode ? 'erase-mode' : ''}`}>
             <button className={`w-[200px] py-4 ${eraseMode ? 'erase-mode' : ''}`}
-               onClick={() => handleButtonClick(1)}
+               onClick={() => handleButtonClick(0)}
             >1-12</button>
              {selectedNumber !== null && (
               <div className="container-chips">
@@ -107,19 +123,15 @@ function Options(props:OptionsProps) {
             <button className="w-[200px] py-4"
             onClick={()=>handlearrayClick(1)}
             >13-24</button>
-            {selectedNumber !== null && (
-              <div className="container-chips">
-                {slots[selectedNumber].coins.map((coin: any, index: number) => (
-                  <Chipsduplicate key={index} color={Color.White}>
-                    {String(coin)}
-                  </Chipsduplicate>
-                ))}
-              </div>
-            )}
+           
           </div>
             <div className="flex">
-                <button className="w-[100px] text-center border border-solid border-white bg-[#2B2A2A] py-4">BLACK</button>
-                <button className="w-[100px] text-center border border-solid border-white bg-[#561589] py-4">PURPLE</button>
+                <button className="w-[100px] text-center border border-solid border-white bg-[#2B2A2A] py-4"
+                onClick={() => handleButtonClickColor(ColorSlot.Gray)}
+                >BLACK</button>
+                <button className="w-[100px] text-center border border-solid border-white bg-[#561589] py-4"
+                  onClick={() => handleButtonClickColor(ColorSlot.Purple)}
+                >PURPLE</button>
             </div>
         </div>
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
