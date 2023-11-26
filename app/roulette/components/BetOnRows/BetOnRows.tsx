6b795e897b62a0c555bb681f02b594031c38c4fa
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2023-11-26 18:33:07 -0500

    1st, 2nd, 3rd buttons functionality

--- app/roulette/components/BetOnRows/BetOnRows.tsx
+++ app/roulette/components/BetOnRows/BetOnRows.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 
 interface RowsProps {
     slots: any[];
@@ -5,15 +7,39 @@ interface RowsProps {
     valueChip?: any;
     eraseMode: boolean
   }
-  
+
 
 function BetOnRows(props:RowsProps) {
     const {slots , setData, valueChip , eraseMode} = props
+    const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+
+    const handleButtonClickPattern = (start: number, end: number) => {
+        updateSlotsPattern(start, end);
+      };
+  
+      const updateSlotsPattern = (start: number, end: number) => {
+        setSelectedNumber(start);
+  
+        const updatedSlots = slots.map((slot, index) => {
+          if (index >= start && index <= end && (index - start) % 3 === 0) {
+            return { ...slot, coins: [...slot.coins, valueChip].slice(-5) };
+          }
+          return slot;
+        });
+  
+        setData(updatedSlots);
+      };
     return (
         <>
-            <button className='w-[50px] h-[70px] border border-solid border-white text-center'>1st</button>
-            <button className='w-[50px] h-[70px] border border-solid border-white'>2st</button>
-            <button className='w-[50px] h-[70px] border border-solid border-white'>3st</button>
+            <button className='w-[50px] h-[70px] border border-solid border-white text-center'
+            onClick={()=> handleButtonClickPattern(1,34)}
+            >1st</button>
+            <button className='w-[50px] h-[70px] border border-solid border-white'
+            onClick={()=> handleButtonClickPattern(2,35)}
+            >2st</button>
+            <button className='w-[50px] h-[70px] border border-solid border-white'
+            onClick={()=> handleButtonClickPattern(3,36)}
+            >3st</button>
         </>
     )
 }
