c123848fdc895a022b9a07e6b18139495df9509b
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2023-11-26 17:55:37 -0500

    refactoring my options component

--- app/roulette/components/Options/Options.tsx
+++ app/roulette/components/Options/Options.tsx
@@ -17,80 +17,54 @@ function Options(props:OptionsProps) {
     const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
     const [selectedColor, setSelectedColor] = useState<ColorSlot | null>(null);
 
-    const handleButtonClick = (number: number) => {
-      setSelectedNumber(number);
+    const handleButtonClick = (start: number, end: number) => {
+      updateSlotsRange(start, end);
+    };
+
+    const updateSlotsRange = (start: number, end: number) => {
+      setSelectedNumber(start);
 
       const updatedSlots = slots.map((slot, index) => {
-        if (index >= 1 && index <= 12) {
-          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
+        if (index >= start && index <= end) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5) };
         }
         return slot;
       });
+
       setData(updatedSlots);
     };
 
-    const handlearrayClick = (number: number) => {
-      setSelectedNumber(number);
-
-      const updatedSlots = slots.map((slot, index) => {
-        if (index >= 13 && index <= 24) {
-          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
-        }
-        return slot;
-      });
-      setData(updatedSlots);
-    };
-
-    const handlearrayfive = (number: number) => {
-      setSelectedNumber(number);
-
-      const updatedSlots = slots.map((slot, index) => {
-        if (index >= 25 && index <= 35) {
-          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
-        }
-        return slot;
-      });
-      setData(updatedSlots);
-    };
-
-    const handlearray = (number: number) => {
-      setSelectedNumber(number);
-
-      const updatedSlots = slots.map((slot, index) => {
-        if (index >= 1 && index <= 18) {
-          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
-        }
-        return slot;
-      });
-      setData(updatedSlots);
-    };
-
-    const handlearraytwo = (number: number) => {
-      setSelectedNumber(number);
-
-      const updatedSlots = slots.map((slot, index) => {
-        if (index >= 19 && index <= 35) {
-          return { ...slot, coins: [...slot.coins, valueChip].slice(-5)};
-        }
-        return slot;
-      });
-      setData(updatedSlots);
-    };
 
     const handleButtonClickColor = (color: ColorSlot) => {
+      updateSlotColor(color);
+    };
+
+    const handleButtonClickColorPurple = (color: ColorSlot) => {
+      updateSlotColor(color);
+    };
+
+    const updateSlotColor = (color: ColorSlot) => {
       setSelectedColor(color);
       const updatedSlots = slots.map((slot) => {
-        if (slot.color === ColorSlot.Gray) {
-          return { ...slot, coins: [...slot.coins,  valueChip ] };
+        if (slot.color === color) {
+          return { ...slot, coins: [...slot.coins, valueChip] };
         }
-        
-       
         return slot;
       });
-
       setData(updatedSlots);
     };
 
+    const handleButtonEvenOdd = (number: number) => {
+      setSelectedNumber(number);
+      const updatedSlots = slots.map((slot, index) => {
+        const isEven = number % 2 === 0;
+        if ((isEven && index % 2 === 0) || (!isEven && index % 2 !== 0)) {
+          return { ...slot, coins: [...slot.coins, valueChip].slice(-5) };
+        }
+        return slot;
+      });
+      setData(updatedSlots);
+    };
 
     console.log(slots)
     return (
@@ -98,7 +72,7 @@ function Options(props:OptionsProps) {
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
           <div className={`${eraseMode ? 'erase-mode' : ''}`}>
             <button className={`w-[200px] py-4 ${eraseMode ? 'erase-mode' : ''}`}
-               onClick={() => handleButtonClick(0)}
+               onClick={() => handleButtonClick(1,12)}
             >1-12</button>
              {selectedNumber !== null && (
               <div className="container-chips">
@@ -113,35 +87,38 @@ function Options(props:OptionsProps) {
           </div>
             <div className="flex">
                 <button className="w-[100px] text-center border border-solid border-white py-4"
-                onClick={()=> handlearray(1)}
+                onClick={()=> handleButtonClick(1,18)}
                 >1-18</button>
-                <button className="w-[100px] text-center border border-solid border-white py-4">EVEN</button>
+                <button className="w-[100px] text-center border border-solid border-white py-4"
+                onClick={()=> handleButtonEvenOdd(2)}
+                >EVEN</button>
             </div>
         </div>
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
           <div>
             <button className="w-[200px] py-4"
-            onClick={()=>handlearrayClick(1)}
+            onClick={()=>handleButtonClick(13,24)}
             >13-24</button>
-           
           </div>
             <div className="flex">
                 <button className="w-[100px] text-center border border-solid border-white bg-[#2B2A2A] py-4"
                 onClick={() => handleButtonClickColor(ColorSlot.Gray)}
                 >BLACK</button>
                 <button className="w-[100px] text-center border border-solid border-white bg-[#561589] py-4"
-                  onClick={() => handleButtonClickColor(ColorSlot.Purple)}
+                  onClick={() => handleButtonClickColorPurple(ColorSlot.Purple)}
                 >PURPLE</button>
             </div>
         </div>
         <div className="flex flex-col w-[200px] justify-center items-center border border-solid border-white">
             <button className="w-[200px] py-4"
-            onClick={()=>handlearrayfive(1)}
+            onClick={()=>handleButtonClick(23,35)}
             >25-35</button>
             <div className="flex">
-                <button className="w-[100px] text-center border border-solid border-white py-4">ODD</button>
                 <button className="w-[100px] text-center border border-solid border-white py-4"
-                onClick={()=>handlearraytwo(1)}
+                onClick={()=>handleButtonEvenOdd(1)}
+                >ODD</button>
+                <button className="w-[100px] text-center border border-solid border-white py-4"
+                onClick={()=>handleButtonClick(19,35)}
                 >19-35</button>
             </div>
         </div>
