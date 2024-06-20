98e35a38874c5f9faace50123e1da187ddad56c8
0xquantum3labs <contact@quantum3labs.com>
2024-06-20 19:57:27 +0800

    fix : remove config files

--- dojoConfig.ts
+++
@@ -1,6 +0,0 @@
-import manifest from "./TheMarquis-contracts/l2/GAMEVAULT-Dojo/target/dev/manifest.json";
-import { createDojoConfig } from "@dojoengine/core";
-
-export const dojoConfig = createDojoConfig({
-  manifest,
-});
