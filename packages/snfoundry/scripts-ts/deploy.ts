90c97041ae741243bae620f097c406e2f229d5c4
jrcarlos2000 <jrcarlos2000.dev@gmail.com>
2024-08-05 15:25:56 +0700

    feat : sync main

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,30 +1,69 @@
 import {
   deployContract,
-  deployer,
   executeDeployCalls,
   exportDeployments,
+  deployer,
 } from "./deploy-contract";
+import { green } from "./helpers/colorize-log";
 
+/**
+ * Deploy a contract using the specified parameters.
+ *
+ * @example (deploy contract with contructorArgs)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       constructorArgs: {
+ *         owner: deployer.address,
+ *       },
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
+ *     }
+ *   );
+ * };
+ *
+ * @example (deploy contract without contructorArgs)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
+ *     }
+ *   );
+ * };
+ *
+ *
+ * @returns {Promise<void>}
+ */
 const deployScript = async (): Promise<void> => {
-  const { address: marquis_core_address } = await deployContract(
-    {
-      owner: deployer.address, // the deployer address is the owner of the contract
+  const { address: marquis_core_address } = await deployContract({
+    contract: "MarquisCore",
+    constructorArgs: {
+      owner: deployer.address,
     },
-    "MarquisCore"
-  );
-  await deployContract(
-    {
+  });
+
+  await deployContract({
+    contract: "Ludo",
+    constructorArgs: {
       marquis_oracle_address: "0xDe3089d40F3491De794fBb1ECA109fAc36F889d0",
       marquis_core_address,
+      owner: deployer.address,
     },
-    "Ludo"
-  );
+  });
 };
 
 deployScript()
-  .then(() => {
+  .then(async () => {
+    await executeDeployCalls();
     exportDeployments();
-    executeDeployCalls().then(() => {});
-    console.log("All Setup Done");
+
+    console.log(green("All Setup Done"));
   })
   .catch(console.error);
