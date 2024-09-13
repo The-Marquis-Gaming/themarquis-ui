915381e0244a591522c1b95146bcde44aeb31410
Buuh2511 <buuhuynh2511@gmail.com>
2024-09-13 15:58:02 +0700

    make matching CI config action

--- packages/nextjs/components/invitation.tsx
+++ packages/nextjs/components/invitation.tsx
@@ -53,7 +53,7 @@ function Invitation() {
     } catch (err) {
       console.error("Failed to generate QR code:", err);
     }
-  }, [codeInvitation])
+  }, [codeInvitation]);
 
   useEffect(() => {
     if (codeInvitation) {
