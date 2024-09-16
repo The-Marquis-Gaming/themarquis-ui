04d10b552357050cece67095910ca7fde127df7a
Buuh2511 <buuhuynh2511@gmail.com>
2024-09-16 09:59:49 +0700

    add middleware

---
+++ packages/nextjs/app/middleware.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  const isAuthenticated = req.cookies.get("accessToken") !== undefined;
+
+  const restrictedRoutes = [
+    "/signup",
+    "/login",
+    "/signup/verification",
+    "/login/verification",
+  ];
+
+  const protectedRoutes = [
+    "/profile",
+    "/login/welcome",
+    "/deposit",
+    "/withdrawal",
+    "/invitation-twitter",
+  ];
+
+  if (
+    isAuthenticated &&
+    restrictedRoutes.some((route) => url.pathname === route)
+  ) {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  if (
+    !isAuthenticated &&
+    protectedRoutes.some((route) => url.pathname.startsWith(route))
+  ) {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/profile",
+    "/signup",
+    "/login/:path*",
+    "/deposit/:path*",
+    "/withdrawal/:path*",
+    "/invitation-twitter",
+  ],
+};
