1e96ebec45a44b3cb5536b4e86d42ceee53004c5
Melnik George <melnik.goshan2013@gmail.com>
2024-12-02 20:50:36 +0300

    Update page.tsx

--- packages/nextjs/app/signup/welcome/page.tsx
+++ packages/nextjs/app/signup/welcome/page.tsx
@@ -22,7 +22,7 @@ function Page() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 1025);
+      setIsMobile(window.innerWidth < 1560);
     };
     handleResize();
     window.addEventListener("resize", handleResize);
@@ -30,18 +30,20 @@ function Page() {
   }, []);
 
   return (
-    <div className="!font-monserrat">
-      <div className="flex flex-col justify-center max-[375px]:gap-0 max-[375px]:pt-0 min-[540px]:pt-4 pt-16 px-2 md:px-6 lg:px-0 gap-4">
+    <div className="!font-monserrat max-w-80 lg:max-w-full mx-auto xl:mx-0">
+      <div
+        className={`flex flex-col justify-center max-[375px]:pt-2 min-[1560px]:pt-16 px-2 lg:px-6 xl:px-0 min-[1560px]:gap-4`}
+      >
         <div className="">
           <BackgroundLogin />
         </div>
-        <div className="flex flex-col lg:flex-row justify-center xl:justify-between items-center max-w-[1700px] mx-auto relative z-40 w-full">
+        <div className="flex flex-col min-[1560px]:flex-row justify-center xl:justify-between items-center max-w-[1700px] mx-auto relative z-40 w-full">
           <div className="flex gap-2 flex-col w-full sm:h-fit h-[400px] justify-between max-w-[800px] items-center">
-            <div className="uppercase xl:flex xl:flex-col items-center">
+          <div className="xl:flex xl:flex-col items-center max-[1560px]:text-center">
               <div className="font-[200] sm:text-3xl text-[18px] text-center">
                 You are now registered
               </div>
-              <div className="sm:text-3xl font-bold text-[18px] text-center">
+              <div className="min-[1560px]:text-4xl !font-monserrat font-bold md:text-3xl text-[18px]">
                 Welcome to{" "}
                 <span className=" text-[#00ECFF] text-gradient">
                   The Marquis,{" "}
@@ -51,17 +53,17 @@ function Page() {
             </div>
             {isMobile && <Invitation />}
             <div
-              className={`flex max-[375px]:mt-2 min-[540px]:mt-2 mt-8 lg:mt-20 w-full  ${isMobile ? "flex-col max-[375px]:gap-2 gap-4 justify-center" : "gap-8 justify-center"}`}
+              className={`flex max-[375px]:mt-2 min-[540px]:mt-2 max-[1560px]:mt-8 mt-20 w-full  ${isMobile ? "flex-col max-[1560px]:gap-2 gap-4 justify-center" : "gap-8 justify-center"}`}
             >
               {isMobile ? (
                 <>
                   <Link
-                    className="shadow-button max-[375px]:py-1 py-4 px-10 font-arcade text-shadow-deposit text-xl text-center"
+                    className="shadow-button max-[1560px]:py-2 py-4 px-10 font-arcade text-shadow-deposit text-xl text-center"
                     href="/"
                   >
                     MAIN
                   </Link>
-                  <button className="bg-[#16828A] shadow-button max-[375px]:py-1 py-4 px-10 font-arcade text-shadow-deposit text-xl">
+                  <button className="bg-[#16828A] shadow-button max-[1560px]:py-2 py-4 px-10 font-arcade text-shadow-deposit text-xl text-center">
                     REMAIN HERE
                   </button>
                 </>
