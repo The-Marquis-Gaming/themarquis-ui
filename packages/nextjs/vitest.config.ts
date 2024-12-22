71e4509678a2b01a85e455a98e2bafff7c82c3a0
AMD <michojekunle1@gmail.com>
2024-12-22 08:36:49 +0100

    fix next test error

--- packages/nextjs/vitest.config.ts
+++ packages/nextjs/vitest.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [],
+  // @ts-ignore 
+  plugins: [react()],
   test: {
     environment: "jsdom",
     include: ["**/*.test.tsx"],
