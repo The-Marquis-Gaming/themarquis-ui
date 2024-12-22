26c7132c4a714dd6ca8d54c4c2e10834297694b0
AMD <michojekunle1@gmail.com>
2024-12-22 08:48:36 +0100

    clean up

--- packages/nextjs/vitest.config.ts
+++ packages/nextjs/vitest.config.ts
@@ -1,6 +1,3 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
