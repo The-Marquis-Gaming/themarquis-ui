740e31d1acd0121fdaa3c2022d04db4ccddb82bc
ekumamatthew <ekumamatthew1@gmail.com>
2024-12-29 19:54:57 +0100

    fix ui lg-screens

--- packages/nextjs/components/Landingpage/PartnershipSection.tsx
+++ packages/nextjs/components/Landingpage/PartnershipSection.tsx
@@ -14,9 +14,9 @@ export default function PartnershipSection() {
     { name: "OpenMark", icon: openMarket },
   ];
   return (
-    <div className="flex w-full lg:px-[32px]  flex-col items-center lg:h-[544px] h-[170px] relative mt-[41.13px] lg:mt-[82.25px] bg-transparent overflow-hidden ">
+    <div className="flex w-full lg:px-[32px] mb-[22px] md:mb-[60px] lg:mb-0 flex-col items-center lg:h-[544px] relative h-[170px] mt-[41.13px] lg:mt-[82.25px] bg-transparent ">
       <div className="lg:h-[166px] h-[43px] w-full flex flex-col items-center">
-        <p className="!font-Larsseit font-medium text-center text-[#9B9B9B] lg:leading-[82.25px] text-[16px] leading-[16px] lg:text-[32px]">
+        <p className="!font-Larsseit font-medium text-center text-[#9B9B9B] lg:leading-[82.25px] text-base lg:text-[32px]">
           Marquis Partnerships
         </p>
         <p className=" !font-Larsseit font-bold text-center text-transparent  text-xl leading-[20px] lg:leading-[56px] lg:text-5xl bg-clip-text bg-gradient-to-t from-white/[0%] via-white to-white">
@@ -24,7 +24,7 @@ export default function PartnershipSection() {
         </p>
       </div>
 
-      <div className="absolute lg:top-[70px]  top-[65px] lg:left-[32px] lg:right-[32px] left-[-13%] right-[0%]">
+      <div className="top-[80px] lg:top-[70px] lg:mt-[-40px] xl:mt-0  lg:relative  h-fit xl:absolute xl:px-[32px] -left-[17%] lg:left-auto right-[0px] absolute overflow-hidden lg:w-full">
         <Swiper
           breakpoints={{
             640: {
@@ -48,10 +48,11 @@ export default function PartnershipSection() {
           loop={true}
           initialSlide={0}
           spaceBetween={22}
+
         >
           {partners?.map((items, index) => {
             return (
-              <SwiperSlide key={index}>
+              <SwiperSlide  key={index}>
                 <div className="w-full items-center justify-center flex">
                   <PartnershipCard text={items.name} icon={items.icon} />
                 </div>
