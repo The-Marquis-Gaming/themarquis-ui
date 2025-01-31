0afbab25310276831184b5f5ecb92c628c8ba9c8
Suhas Ghosal <63814211+suhas-sensei@users.noreply.github.com>
2025-01-31 17:51:10 +0000

    deposit function tests

--- packages/nextjs/components/scaffold-stark/CustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-stark/CustomConnectButton/index.tsx
@@ -5,33 +5,26 @@ import { AddressQRCodeModal } from "./AddressQRCodeModal";
 import { WrongNetworkDropdown } from "./WrongNetworkDropdown";
 import { useAutoConnect } from "~~/hooks/scaffold-stark";
 import { useAccount } from "@starknet-react/core";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import ConnectModal from "./ConnectModal";
 import scaffoldConfig from "~~/scaffold.config";
 import { Address } from "@starknet-react/chains";
 import { getBlockExplorerAddressLink } from "~~/utils/scaffold-stark";
 import { useTargetNetwork } from "~~/hooks/scaffold-stark/useTargetNetwork";
 
-/**
- * Custom Connect Button (watch balance + custom design)
- */
 export const CustomConnectButton = () => {
   useAutoConnect();
   const { targetNetwork } = useTargetNetwork();
-  const { status, address: accountAddress } = useAccount();
-  const [connectedChainId, setConnectedChainId] = useState<bigint>();
+  const { status, address: accountAddress, chainId } = useAccount();
 
   const blockExplorerAddressLink = useMemo(() => {
-    return (
-      accountAddress &&
-      getBlockExplorerAddressLink(targetNetwork, accountAddress)
-    );
+    return accountAddress && getBlockExplorerAddressLink(targetNetwork, accountAddress);
   }, [accountAddress, targetNetwork]);
 
   if (status === "disconnected") return <ConnectModal />;
-  
-  // Skip wrong network check if using a fork
-  if (!scaffoldConfig.isFork && connectedChainId !== targetNetwork.id) {
+
+  // Skip wrong network check if using a fork or on devnet
+  if (!scaffoldConfig.isFork && chainId && chainId !== targetNetwork.id && targetNetwork.id !== 1n) {
     return <WrongNetworkDropdown />;
   }
 
