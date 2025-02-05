e9afb7ff54cfc7a5c780eb86c34a8aed08b54b28
Suhas Ghosal <63814211+suhas-sensei@users.noreply.github.com>
2025-02-05 18:51:02 +0000

    updated useScaffoldStrkBalance.ts

--- packages/nextjs/hooks/scaffold-stark/useScaffoldStrkBalance.ts
+++ packages/nextjs/hooks/scaffold-stark/useScaffoldStrkBalance.ts
@@ -4,8 +4,6 @@ import { useReadContract } from "@starknet-react/core";
 import { BlockNumber } from "starknet";
 import { Abi } from "abi-wan-kanabi";
 import { formatUnits } from "ethers";
-import { useMemo } from "react";
-import { strkTokenAbi } from "~~/utils/Constants";
 
 type UseScaffoldStrkBalanceProps = {
   address?: Address | string;
@@ -14,18 +12,13 @@ type UseScaffoldStrkBalanceProps = {
 const useScaffoldStrkBalance = ({ address }: UseScaffoldStrkBalanceProps) => {
   const { data: deployedContract } = useDeployedContractInfo("Strk");
 
-  // Memoize args to avoid unnecessary updates
-  const args = useMemo(() => (address ? [address] : []), [address]);
-
   const { data, ...props } = useReadContract({
     functionName: "balance_of",
-    address:
-      deployedContract?.address ||
-      "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
-    abi: (deployedContract?.abi || strkTokenAbi) as Abi as any[],
-    watch: false, // Disable watch as we control updates through `enabled`
-    enabled: Boolean(address), // Only enable if address is defined
-    args,
+    address: deployedContract?.address,
+    abi: deployedContract?.abi as Abi as any[],
+    watch: true,
+    enabled: true,
+    args: address ? [address] : [],
     blockIdentifier: "pending" as BlockNumber,
   });
 
